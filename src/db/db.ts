fc360e36e92060fa5c903b5ffdc8b729f3c7c398
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-06-16 20:37:29 +0500

    9

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,34 +4,35 @@ import { DB } from "..";
 export const db: DB = {
   blogs: [
     {
-      "id": "0",
-      "name": "string",
-      "description": "string",
-      "websiteUrl": "string"
+      id: "0",
+      name: "string",
+      description: "string",
+      websiteUrl: "string"
     },
 
     {
-      "id": "1",
-      "name": "string",
-      "description": "string",
-      "websiteUrl": "string"
+      id: "1",
+      name: "string",
+      description: "string",
+      websiteUrl: "string"
     }
   ],
-  posts: [    {
-    "id": "Leva",
-    "title": "First steps",
-    "shortDescription": "string",
-    "content": "string",
-    "blogId": "string",
-    "blogName": "string"
+  posts: [    
+    {
+    id: "Leva",
+    title: "First steps",
+    shortDescription: "string",
+    content: "string",
+    blogId: "string",
+    blogName: "string"
 },
 
 {
-    "id": "Platon",
-    "title": "First words",
-    "shortDescription": "string",
-    "content": "string",
-    "blogId": "string",
-    "blogName": "string"
+    id: "Platon",
+    title: "First words",
+    shortDescription: "string",
+    content: "string",
+    blogId: "string",
+    blogName: "string"
 }]
 };
